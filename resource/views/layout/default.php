8aa9db336038ab1e27964975fa8e3ea206f6652a
AndreyDuda <andreyduda@gmail.com>
2019-07-01 22:44:38 +0300

    new part

--- resource/views/layout/default.php
+++ resource/views/layout/default.php
@@ -9,10 +9,9 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<?php echo $this->getMeta(); ?>
+	<?php echo $this->getMeta();?>
 </head>
 <body>
-<h1>Ш-А-Б-Л-О-Н = D-E-F-A-U-L-T</h1>
 <?php echo $content;?>
 </body>
 </html>
