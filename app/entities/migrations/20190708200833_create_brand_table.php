2b82dfcaf806041b511da57a1c5cc814b29dd0b4
AndreyDuda <AndreyDuda88@gmail.com>
2019-07-08 23:39:46 +0300

    add first migrate "brands" and edit template for mograte at phinx

---
+++ app/entities/migrations/20190708200833_create_brand_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateBrandTable extends AbstractMigration
+{
+    /**
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    addCustomColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+
+    public function up()
+    {
+        $this->table('brands')
+            ->addColumn('title', 'string', ['limit' => 255, 'null' => false])
+            ->addColumn('alias', 'string', ['limit' => 255, 'null' => false, ])
+            ->addColumn('img', 'string', ['limit' => 255,'default' => 'brand_default.jpg', 'null' => false])
+            ->addColumn('description', 'string', ['limit' => 255, 'null' => false])
+            ->addIndex(['alias'], ['unique' => true])
+        ;
+    }
+
+    public function down()
+    {
+        $this->table('brands')->drop();
+    }
+}
